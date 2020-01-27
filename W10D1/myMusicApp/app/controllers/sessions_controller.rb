a64463f5de946db31c0ab35528e9e3dd9ccf3b2a
JayasreeSuryadevara <sjayas@hotmail.com>
2020-01-27 12:34:19 -0800

    myMusicApp Added UserModel and Login function

---
+++ W10D1/myMusicApp/app/controllers/sessions_controller.rb
@@ -0,0 +1,28 @@
+class SessionsController < ApplicationController
+
+    def new
+        render :new
+    end
+
+    def create
+        user = User.find_by_credentials(
+            params[:user][:email], 
+            params[:user][:password]
+        )
+
+        if user.nil?
+            Flash.now[:errors] =["Incorrect Email/Password!"]
+            render :new
+        else 
+            log_in_user!(user)
+            # redirect_to music_url
+        end
+
+    end
+
+    def destroy
+        log_out_user!
+        redirect_to new_session_url
+    end
+
+end
---
+++ W10D1/myMusicApp/app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+    
+    def new
+        @user = User.new
+        render :new
+    end
+
+    def create
+        user = User.new(user_params)
+
+        if user.save
+            # redirect_to music_url
+        else
+            flash.now[:errors] = "Email address already exists!"
+            render :new
+        end
+
+    end
+
+    def destroy
+
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit( :email, :password)
+    end
+    
+end
