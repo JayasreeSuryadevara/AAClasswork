2ba0da3cf887771fefc4228cfb1ec91de8d6d4c7
JayasreeSuryadevara <sjayas@hotmail.com>
2020-02-19 17:20:42 -0800

    Todos upto Phase 2

---
+++ W13D3/Todos/store/store.js
@@ -0,0 +1,12 @@
+import { createStore} from 'redux';
+import rootReducer from '../reducers/root_reducer';
+
+const configureStore = () => {
+    return createStore(rootReducer);
+}
+
+export default configureStore;
+
+//store.dispatch({ action, body })
+
+//store.getState
