b6b66ac7bed10be249b58723babcf487970f5f80
JayasreeSuryadevara <sjayas@hotmail.com>
2020-04-22 15:15:01 -0700

    Tabulation Project

---
+++ Algorithms/tabulation_project/lib/leet_code_70.js
@@ -0,0 +1,13 @@
+// Work through this problem on https://leetcode.com/problems/climbing-stairs/ and use the specs given there.
+// Feel free to use this file for scratch work.
+
+var climbStairs = function (n) {
+  let table = new Array(n);
+  table[0] = 1;
+  table[1] = 1;
+  for (let i = 2; i <= n; i++) {
+    table[i] = table[i - 1] + table[i - 2];
+  }
+  return table[n];
+
+};
